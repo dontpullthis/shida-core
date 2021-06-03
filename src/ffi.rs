39ca5472698b50fe7524e40c4b9474d32b1597b3
Sergei Belyakov <1899416+tasmanianfox@users.noreply.github.com>
2021-06-03 22:45:52 +0300

    Defined reader functions

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,14 +1,14 @@
 use std::ffi::CString;
 use std::ffi::CStr;
 
-#[allow(non_camel_case_types)]
-pub type const_c_char_ptr = *const libc::c_char;
+pub type ConstCCharPtr = *const libc::c_char;
+pub type Size = libc::size_t;
 
-pub unsafe fn string_to_ccharptr(string: String) -> *const libc::c_char {
+pub unsafe fn string_to_ccharptr(string: String) -> ConstCCharPtr {
     CString::from_vec_unchecked(Vec::from(string.as_bytes())).into_raw()
 }
 
-pub unsafe fn ccharptr_to_string(ccharptr: *const libc::c_char) -> Result<String, std::str::Utf8Error> {
+pub unsafe fn ccharptr_to_string(ccharptr: ConstCCharPtr) -> Result<String, std::str::Utf8Error> {
     let cstr = CStr::from_ptr(ccharptr);
     
     match cstr.to_str() {
--- src/module.rs
+++ src/module.rs
@@ -1,6 +1,17 @@
-#[allow(unused_imports)]
-type CanHandleFunc = fn(connection_type: *const libc::c_char) -> bool;
+use crate::ffi::{ConstCCharPtr, Size};
 
+#[allow(unused_imports)]
+pub type CanHandleFunc = fn(connection_type: ConstCCharPtr) -> bool;
+
+#[allow(unused_imports)]
+type InitReaderFunc = fn(paramsc: Size, paramsv: *const ConstCCharPtr) -> (*const u8, ConstCCharPtr);
+
+#[allow(unused_imports)]
+type ReadFunc = fn(conn_ptr: *const u8) -> (ConstCCharPtr, ConstCCharPtr);
+
+#[repr(C)]
 pub struct Module {
     pub can_handle: CanHandleFunc,
+    pub init_reader: InitReaderFunc,
+    pub read: ReadFunc
 }
