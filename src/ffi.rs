192b42fb9d58d734bb7f1054c3d93592a377ff22
Sergei Belyakov <1899416+tasmanianfox@users.noreply.github.com>
2021-06-09 00:58:13 +0300

    Implemented common objects to pass params

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,8 +1,21 @@
 use std::ffi::CString;
 use std::ffi::CStr;
+use std::mem;
 
 pub type ConstCCharPtr = *const libc::c_char;
+pub type MutCCharPtr = *mut libc::c_char;
 pub type Size = libc::size_t;
+
+pub unsafe fn bytes_vec_to_ccharptr(input: &Vec<u8>) -> MutCCharPtr {
+    CString::from_vec_unchecked(input.to_vec()).into_raw()
+}
+
+pub fn ccharptr_to_bytes_vec(input: ConstCCharPtr) -> Vec<u8> {
+    let c_str: &CStr = unsafe { CStr::from_ptr(input) };
+    let str_slice: &str = c_str.to_str().unwrap();
+    Vec::from(str_slice)
+}
+
 
 pub unsafe fn string_to_ccharptr(string: String) -> ConstCCharPtr {
     CString::from_vec_unchecked(Vec::from(string.as_bytes())).into_raw()
@@ -15,4 +28,8 @@ pub unsafe fn ccharptr_to_string(ccharptr: ConstCCharPtr) -> Result<String, std:
         Ok(s) => Ok(String::from(s)),
         Err(e) => Err(e),
     }
+}
+
+pub unsafe fn malloc<T>(len: usize) -> *mut T {
+    libc::malloc(mem::size_of::<T>() * len) as *mut T
 }
